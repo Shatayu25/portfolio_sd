87fd65834b734e9f3b42c15d7cf48e3816890846
Shatayu_25 <shatayudeshpande99@gmail.com>
2026-08-24 19:37:41 +0530

    Bug Fixes

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -4,7 +4,7 @@ export const projectsData = [
     link: "https://the-wild-oasis-sd.vercel.app/",
     description:
       "The Wild Oasis is a feature-rich React application built for hotel staff. Designed to handle real-world data fetching and complex UI state, it features a live interactive dashboard, secure authentication, and a modern dark-mode UI. It relies on React Query for advanced server-state caching and Supabase as a headless backend API.",
-    projectImg: "./project_img.svg",
+    projectImg: "./project_img.png",
     techStack: [
       { img: "./react_logo.svg", alt: "react_logo", name: "React" },
       { img: "./rq-logo.svg", alt: "rq-logo", name: "React-Query" },
